7551a28a3652892732cffb6b236f1661397c1312
Atilla  (Home) <atilla@kati.bz>
2022-10-03 20:22:17 +0200

    Lösung - Vorlage

---
+++ src/20220929/Eingaben_GL/EingabenBeispiel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EingabenBeispiel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Schreiben Sie eine Applikation, mit der die Fläche und 
+            * der Umfang eines unregelmässigen Dreiecks berechnet werden kann.
+            * 
+            * - Sämtliche Seiten-Längen sollen von einem User eingegeben werden können.
+            * - Ergebnisse und Eingaben sollen am Ende in einer ansprechenden Form 
+            *   dargestellt werden.
+            * - die Applikation soll Fehlertolerant sein (keine Exceptions)
+            * - verwenden Sie Farben in der Ausgabe
+            */
+
+            //Löusing Flächenberechnung nach Heron: https://www.arndt-bruenner.de/mathe/9/herondreieck.htm
+
+            //U = a + b + c
+            //s = U/2
+            //A = SQRT( s * (s-a) * (s-b) * (s-c) )
+        }
+    }
+}
