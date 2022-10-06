cd2dea05e3283ab556a2f6b71eec488f82d36128
Atilla  (Home) <atilla@kati.bz>
2022-10-06 20:10:05 +0200

    Beispiel Iterationen

---
+++ src/20221006/Iterationen/Iterationen/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterationen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Gandalf";
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Aktueller Wert: {i}");
+                i--;
+
+                i = 5;
+            }
+            
+            //int i;
+            //int j = 3;
+            //for (i = 0, Console.WriteLine($"Start: i={i}, j={j}"); i < j; i++, j--, Console.WriteLine($"Step: i={i}, j={j}"))
+            //{
+            //    //...
+            //}
+
+            bool inputIsValid = false;
+
+            while (!inputIsValid)
+            {
+                Console.WriteLine("Tu was...");
+                inputIsValid = true;
+            }
+
+            do
+            {
+                Console.WriteLine("Tu was...");
+                inputIsValid = true;
+            }
+            while (!inputIsValid);
+
+
+        }
+    }
+}
---
+++ src/20221006/Iterationen/TeilnehmerVerwaltungV1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeilnehmerVerwaltungV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * Wir wollen eine kleine Applikation zur (Kurs)Teilnehmerverwaltung schreiben.
+             * 1.) Folgende Daten pro Teilnehmer sollen eingelesen werden:
+             * 
+             * - Name + Vorname
+             * - Geburtsdatum
+             * - Wohnort
+             * 
+             * 2.) Es sollen beliebig viele Teilnehmer erfasst werden.
+             * 
+             * 3.) Teilnehmer-Daten (=alle Daten pro Session in eine Datei) 
+             *     sollen in einer csv-Datei gespeichert werden.
+             *     Aufbau der csv-Datei:
+             *     [Vorname],[Nachname],[Geb.Datum],[Wohnort]
+             *     
+             * */
+
+            //schreiben in eine Datei:
+            StreamWriter sw = new StreamWriter("teilnehmerdaten.csv", true);
+            sw.WriteLine();  //schreibt eine Zeile inkl. Zeilneumbruch
+            Console.WriteLine();
+
+            sw.Close();
+
+            /* 4.) autom. Altersbeschränkung einbauen ==> T > 18 < 110 Jahre
+             * 5.) Fehlertolerante Umsetzung, d.h. Fehleingaben führen zu Wiederholung
+             * 
+             * 
+             */
+        }
+    }
+}
