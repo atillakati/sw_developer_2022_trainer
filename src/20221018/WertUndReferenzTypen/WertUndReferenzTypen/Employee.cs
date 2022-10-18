dd985177a2d2281bd1b84feb37da1eb449526723
Atilla  (Home) <atilla@kati.bz>
2022-10-18 19:18:59 +0200

    Wert- und Referenz Typen

---
+++ src/20221018/WertUndReferenzTypen/WertUndReferenzTypen/Employee.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WertUndReferenzTypen
+{
+    internal class Employee
+    {
+        private string _name;
+        private Guid _id;
+        private DateTime _birthday;
+        public decimal _salary;
+
+        //std. Konstruktor
+        //public Employee() 
+        //{
+        //    Name = "Max Mustermann";
+        //    Id = Guid.NewGuid();
+        //    Birthday = DateTime.MinValue;
+        //    Salary = 800;
+        //}
+
+        //benutz. Konstruktor
+        public Employee(string name, DateTime birthday)
+        {
+            _name = name;
+            _birthday = birthday;
+
+            _id = Guid.NewGuid();
+            _salary = 800;
+        }
+
+        ////Änderungs- und Zugriffsmethoden
+        //public decimal get_Salary()
+        //{
+        //    return Salary;
+        //}
+
+        //public string get_Name()
+        //{
+        //    return Name;
+        //}
+
+        //public void set_Name(string newName)
+        //{
+        //    if (!string.IsNullOrEmpty(newName))
+        //    {
+        //        Name = newName;
+        //    }
+        //}
+
+        //public Guid GetId()
+        //{
+        //    return Id;
+        //}
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public void AdaptSalary(decimal delta)
+        {
+            if (delta > 1000 || delta < -1000 || _salary + delta < 0)
+            {
+                return;
+            }
+
+            _salary += delta;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"ID: {_id} - {_name}");
+            Console.WriteLine($"Alter: {DateTime.Now.Year - _birthday.Year} Gehalt: EUR {_salary:f2}");
+        }
+    }
+}
---
+++ src/20221018/WertUndReferenzTypen/WertUndReferenzTypen/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WertUndReferenzTypen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1 = 5;
+            int zahl2 = 8;
+
+            zahl1 = zahl2;
+            zahl2 = 2;
+
+            Employee ma1 = new Employee("Mitarbeiter1", DateTime.Now);
+            Employee ma2 = new Employee("Mitarbeiter2", DateTime.Now);
+
+            ma1._salary = 500.0m;
+            ma2._salary = 1500.0m;
+
+            ma1 = ma2;
+            ma2._salary = 2000.0m;
+
+            Display(ma1);
+
+            UpdateName(ma1);
+
+            if(ma1 == ma2)
+            {
+
+            }
+
+            Employee ma3;
+
+            ma3._salary = 1500.0m;
+
+            Console.WriteLine($"Zahl 1: {ma1._salary} Zahl 2: {ma2._salary}");
+
+        }
+
+        private static Employee UpdateName(Employee maToChange)
+        {
+            maToChange._salary = 0.0m;
+
+            return maToChange;
+        }
+
+        private static void Display(Employee ma)
+        {
+            Employee mb = new Employee("Mitarbeiter1", DateTime.Now);
+
+            mb = ma;
+        }
+    }
+}
