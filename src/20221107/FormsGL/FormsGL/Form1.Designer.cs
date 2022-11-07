65e52fe131f959751e6915494e2eec68c0dd9449
Atilla  (Home) <atilla@kati.bz>
2022-11-07 21:31:44 +0100

    Forms GL

---
+++ src/20221107/FormsGL/FormsGL/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace FormsGL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btt_ok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_input = new System.Windows.Forms.TextBox();
+            this.lbl_ouputText = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btt_ok
+            // 
+            this.btt_ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btt_ok.Location = new System.Drawing.Point(238, 202);
+            this.btt_ok.Name = "btt_ok";
+            this.btt_ok.Size = new System.Drawing.Size(141, 28);
+            this.btt_ok.TabIndex = 0;
+            this.btt_ok.Text = "Übernehmen";
+            this.btt_ok.UseVisualStyleBackColor = true;
+            this.btt_ok.Click += new System.EventHandler(this.btt_ok_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(44, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name: ";
+            // 
+            // txt_input
+            // 
+            this.txt_input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_input.Location = new System.Drawing.Point(123, 37);
+            this.txt_input.Name = "txt_input";
+            this.txt_input.Size = new System.Drawing.Size(653, 20);
+            this.txt_input.TabIndex = 2;
+            // 
+            // lbl_ouputText
+            // 
+            this.lbl_ouputText.AutoSize = true;
+            this.lbl_ouputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ouputText.Location = new System.Drawing.Point(164, 72);
+            this.lbl_ouputText.Name = "lbl_ouputText";
+            this.lbl_ouputText.Size = new System.Drawing.Size(66, 24);
+            this.lbl_ouputText.TabIndex = 3;
+            this.lbl_ouputText.Text = "label2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btt_ok);
+            this.groupBox1.Location = new System.Drawing.Point(123, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(394, 257);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 535);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_ouputText);
+            this.Controls.Add(this.txt_input);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Test Applikation";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btt_ok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_input;
+        private System.Windows.Forms.Label lbl_ouputText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
