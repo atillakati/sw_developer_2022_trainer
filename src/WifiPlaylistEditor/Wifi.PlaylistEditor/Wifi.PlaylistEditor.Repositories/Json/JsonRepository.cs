6faae45914f769b50be110e188c274a7b9aafda0
Atilla  (Home) <atilla@kati.bz>
2022-11-17 21:28:15 +0100

    Add JsonRepository

---
+++ src/WifiPlaylistEditor/Wifi.PlaylistEditor/Wifi.PlaylistEditor.Repositories/Json/JsonRepository.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.PlaylistEditor.Types;
+
+namespace Wifi.PlaylistEditor.Repositories.Json
+{
+    public class JsonRepository : IRepository
+    {
+        public string Extension => ".json";
+
+        public string Description => "Wifi playlist format";
+
+        public IPlaylist Load(string playlistFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IPlaylist playlist, string playlistFilePath)
+        {
+            var entity = playlist.ToEntity();
+
+            string json = JsonConvert.SerializeObject(entity);
+
+        }
+    }
+}
